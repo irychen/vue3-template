495be19d5242863708892444de2fb6781c895524
RychenWong <wongyichen@163.com>
2022-11-14 09:48:50 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 import { NConfigProvider } from 'naive-ui';
 import { toRefs } from 'vue';
 import { useThemeStore } from './store/themeStore';
+
 const themeStore = useThemeStore();
 const { NThemeConfig } = toRefs(themeStore);
 </script>
 
 <template>
     <n-config-provider :theme-overrides="NThemeConfig">
-      <router-view />
+        <router-view />
     </n-config-provider>
 </template>
 
